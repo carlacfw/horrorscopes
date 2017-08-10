ddfd67498214cf89d11cb3d9dd44f10661976014
Michael Keogh <brookemossburn@gmail.com>
2017-08-10 13:29:17 +1200

    moved webpack to postinstall

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
-var createServer = require('./server')
 
 var Knex = require('knex')
 var config = require('../knexfile')[process.env.NODE_ENV || 'development']
 var knex = Knex(config)
 
+var createServer = require('./server')
 var server = createServer(knex)
 var PORT = process.env.PORT || 3000
 
